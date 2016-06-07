0bdadcefe7c8f467021df08d451695276efe5995
Matt Carter <m@ttcarter.com>
2016-06-07 10:59:53 +1000

    Update 2016-06-07T00:59:46.033Z

---
+++ js/angular-ui-flag.js
@@ -0,0 +1,148 @@
+angular.module('angular-ui-flag', [])
+.directive('uiFlag', function($http) {
+	return {
+		scope: {
+			style: '=',
+		},
+		restrict: 'AE',
+		template: 
+			'<svg width="{{style.frame.width}}" height="{{style.frame.height}}">' +
+				'<g class="ui-flag-background"></g>' +
+				'<g class="ui-flag-foreground"></g>' +
+				'<g class="ui-flag-feature"></g>' +
+			'</svg>',
+		controller: function($scope) {
+			$scope.elementSections; // SVG groups of each aspect of the flag (background, foreground + feature)
+
+			// Utility functions {{{
+			$scope.splitColors = function(element) {
+				var output = [[],[],[]];
+
+				angular.element(element).find('path')
+					.filter(function(key, elem) {
+						var ae = angular.element(elem);
+						var fill = ae.css('fill');
+						var colorNo = false;
+
+						if (ae.attr('colorNo')) { // Categorised previously
+							colorNo = ae.attr('colorNo');
+						} else if (fill == 'rgb(255, 0, 0)') {
+							colorNo = 0;
+						} else if (fill == 'rgb(0, 255, 0)') {
+							colorNo = 1;
+						} else if (fill == 'rgb(0, 0, 255)') {
+							colorNo = 2;
+						}
+
+						if (colorNo !== false) {
+							ae.attr('colorNo', colorNo);
+							output[colorNo].push(ae);
+						}
+					});
+
+				return output;
+			};
+			// }}}
+
+			// Redrawing {{{
+			// Background {{{
+			$scope._lastBackground;
+			$scope.redrawBackground = function() {
+				if ($scope.style.background.svg == $scope._lastBackground) return $scope.styleBackground(); // No need to reload - just restyle
+				$http.get($scope.style.background.svg)
+					.then(function(res) {
+						$scope.elementSections.background.innerHTML = res.data;
+						var newSVG = angular.element($scope.elementSections.background.children[0]);
+						newSVG.attr({
+							width: $scope.style.frame.width,
+							height: $scope.style.frame.height,
+						});
+						$scope.styleBackground();
+						$scope._lastBackground = $scope.style.background.svg;
+					})
+			};
+
+			$scope.styleBackground = function() {
+				var splits = $scope.splitColors($scope.elementSections.background.children[0]);
+
+				splits[0].forEach(function(elem) { elem.css('fill', $scope.style.background.color1) });
+				splits[1].forEach(function(elem) { elem.css('fill', $scope.style.background.color2) });
+				splits[2].forEach(function(elem) { elem.css('fill', $scope.style.background.color3) });
+			};
+			// }}}
+
+			// Foreground {{{
+			$scope.lastForeground;
+			$scope.redrawForeground = function() {
+				if ($scope.style.foreground.svg == $scope._lastForeground) return $scope.styleForeground(); // No need to reload - just restyle
+				$http.get($scope.style.foreground.svg)
+					.then(function(res) {
+						$scope.elementSections.foreground.innerHTML = res.data;
+						var newSVG = angular.element($scope.elementSections.foreground.children[0]);
+						newSVG.attr({
+							width: $scope.style.frame.width,
+							height: $scope.style.frame.height,
+						});
+						$scope.styleForeground();
+						$scope._lastForeground = $scope.style.foreground.svg;
+					})
+			};
+
+			$scope.styleForeground = function() {
+				var splits = $scope.splitColors($scope.elementSections.foreground.children[0]);
+
+				splits[0].forEach(function(elem) { elem.css('fill', $scope.style.foreground.color1) });
+				splits[1].forEach(function(elem) { elem.css('fill', $scope.style.foreground.color2) });
+				splits[2].forEach(function(elem) { elem.css('fill', $scope.style.foreground.color3) });
+			};
+			// }}}
+
+			// Feature {{{
+			$scope._lastFeature;
+			$scope.redrawFeature = function() {
+				// if ($scope.style.feature.svg == $scope._lastFeature) return $scope.styleFeature(); // No need to reload - just restyle
+				$http.get($scope.style.feature.svg)
+					.then(function(res) {
+						var boundingElem = angular.element($scope.elementSections.background).find('#feature');
+						if (!boundingElem.length) return console.warn('Cannot find #feature ID within background', $scope.style.background.svg);
+						// console.log('BOUND ELEM', boundingElem[0]);
+						// console.log('BOUND', boundingElem.attr('x'), boundingElem.attr('y'), boundingElem.attr('width'), boundingElem.attr('height'));
+
+						$scope.elementSections.feature.innerHTML = res.data;
+						var newSVG = angular.element($scope.elementSections.feature.children[0]);
+						newSVG.attr({
+							x: boundingElem.attr('x') || '0',
+							y: boundingElem.attr('y') || '0',
+							width: boundingElem.attr('width') || $scope.style.frame.width,
+							height: boundingElem.attr('height') || $scope.style.frame.height,
+						});
+						$scope.styleFeature();
+						$scope._lastFeature = $scope.style.feature.svg;
+					})
+			};
+
+			$scope.styleFeature = function() {
+				var splits = $scope.splitColors($scope.elementSections.feature);
+
+				splits[0].forEach(function(elem) { elem.css('fill', $scope.style.feature.color1) });
+				splits[1].forEach(function(elem) { elem.css('fill', $scope.style.feature.color2) });
+				splits[2].forEach(function(elem) { elem.css('fill', $scope.style.feature.color3) });
+			};
+			// }}}
+			// }}}
+
+			// Watching + trigger redrawing {{{
+			$scope.$watchGroup(['style.background.svg', 'style.background.color1', 'style.background.color2', 'style.background.color3'], $scope.redrawBackground);
+			$scope.$watchGroup(['style.foreground.svg', 'style.foreground.color1', 'style.foreground.color2', 'style.foreground.color3'], $scope.redrawForeground);
+			$scope.$watchGroup(['style.feature.svg', 'style.background.svg', 'style.feature.color1', 'style.feature.color2', 'style.feature.color3'], $scope.redrawFeature);
+			// }}}
+		},
+		link: function($scope, elem, attr, ctrl) {
+			$scope.elementSections = {
+				background: elem.find('.ui-flag-background')[0],
+				foreground: elem.find('.ui-flag-foreground')[0],
+				feature: elem.find('.ui-flag-feature')[0],
+			};
+		},
+	}
+});
---
+++ js/app.js
@@ -0,0 +1,177 @@
+var app = angular.module('app', [
+	'angular-ui-flag',
+	'colorpicker.module',
+]);
+
+app.controller('flagExampleController', function($scope, $http, $interval, $q) {
+	// .flagStyle - flag drawing options {{{
+	$scope.flagStyle = {
+		frame: {
+			width: 500,
+			height: 300,
+		},
+		background: {
+			svg: '',
+			color1: '#FF0000',
+			color2: '#00FF00',
+			color3: '#0000FF',
+		},
+		foreground: {
+			svg: '',
+			color1: '#FFFF00',
+			color2: '#FF00FF',
+			color3: '#FFFFFF',
+		},
+		feature: {
+			svg: '',
+			color1: '#FFFF00',
+			color2: '#FF00FF',
+			color3: '#FFFFFF',
+		},
+	};
+	// }}}
+
+	// .colors - list of nice colors to choose from {{{
+	$scope.colors = [
+		'#be252e', // Red
+		'#587bc6', // Light blue
+		'#31336b', // Dark blue
+		'#e4d12f', // Yellow
+		'#dc692b', // Orange
+		'#4a9d4c', // Green
+		'#000000', // Black
+		'#ffffff', // White
+	];
+	// }}}
+	
+	// .options - available options for each flag part {{{
+	$scope.options = {
+		activeBackground: null,
+		activeForeground: null,
+		activeFeature: null,
+		backgrounds: [], // Loaded from svg/bg/index.json
+		foregrounds: [],
+		features: [],
+	};
+
+	$scope.loading = true;
+	$q.all([
+		$http.get('/svg/bg/index.json')
+			.then(function(res) {
+				$scope.options.backgrounds = res.data;
+			}),
+
+		$http.get('/svg/fg/index.json')
+			.then(function(res) {
+				$scope.options.foregrounds = res.data;
+			}),
+
+		$http.get('/svg/ft/index.json')
+			.then(function(res) {
+				$scope.options.features = res.data;
+			}),
+	])
+		.then(function() {
+			$scope.loading = false;
+		});
+	// }}}
+
+	// Set options.active{background|foreground|feature} trackers {{{
+	$scope.$watch('flagStyle.background.svg', function() {
+		$scope.options.activeBackground = _.find($scope.options.backgrounds, {file: $scope.flagStyle.background.svg});
+	});
+
+	$scope.$watch('flagStyle.foreground.svg', function() {
+		$scope.options.activeForeground = _.find($scope.options.foregrounds, {file: $scope.flagStyle.foreground.svg});
+	});
+
+	$scope.$watch('flagStyle.feature.svg', function() {
+		$scope.options.activeFeature = _.find($scope.options.features, {file: $scope.flagStyle.feature.svg});
+	});
+	// }}}
+
+	// .set() - utility function to set a path (via lodash _.set) {{{
+	$scope.set = function(path, value) {
+		_.set($scope, path, value);
+	};
+	// }}}
+
+	// .toggle() - utility function to otggle a path (via lodash _.set) {{{
+	$scope.toggle = function(path) {
+		_.set($scope, path, !_.get($scope, path));
+	};
+	// }}}
+
+	// Timer {{{
+	$scope.timerInterval = 2000;
+	$scope.timerEnabled = false;
+	$scope.timerHandle;
+	$scope.$watch('timerEnabled', function() {
+		if ($scope.timerEnabled) {
+			$scope.timerHandle = $interval($scope.randomizeAll, $scope.timerInterval);
+		} else {
+			$interval.cancel($scope.timerHandle);
+		}
+	});
+	// }}}
+
+	// Randomizers {{{
+	$scope.randomizeAll = function() {
+		$scope.randomizeBackground();
+		$scope.randomizeForeground();
+		$scope.randomizeFeature();
+	};
+
+	$scope.randomizeBackground = function() {
+		$scope.flagStyle.background = {
+			svg: _.sample($scope.options.backgrounds).file,
+			color1: _.sample($scope.colors),
+			color2: _.sample($scope.colors),
+			color3: _.sample($scope.colors),
+		};
+	};
+
+	$scope.randomizeForeground = function() {
+		$scope.flagStyle.foreground = {
+			svg: _.sample($scope.options.foregrounds).file,
+			color1: _.sample($scope.colors),
+			color2: _.sample($scope.colors),
+			color3: _.sample($scope.colors),
+		};
+	};
+
+	$scope.randomizeFeature = function() {
+		$scope.flagStyle.feature = {
+			svg: _.sample($scope.options.features).file,
+			color1: _.sample($scope.colors),
+			color2: _.sample($scope.colors),
+			color3: _.sample($scope.colors),
+		};
+	};
+	// }}}
+
+	// Workaround for mobiles {{{
+	$(function() {
+		$(window)
+			.on('resize', function() {
+				if ($(window).width() < 500) {
+					$('.well').css('padding', '2px');
+					$('ui-flag > svg > g')
+						.attr('transform', 'scale(0.7, 0.7)');
+				} else {
+					$('.well').css('padding', '5px');
+					$('ui-flag > svg > g')
+						.attr('transform', 'scale(1, 1)');
+				}
+			})
+			.trigger('resize');
+	});
+	// }}}
+
+	// Kick off all ranomizers on load {{{
+	$scope.$watchGroup(['options.backgrounds', 'options.foregrounds', 'options.features'], function() {
+		if (!$scope.options.backgrounds.length || !$scope.options.foregrounds.length || !$scope.options.features.length) return; // Load yet loaded everything
+		$scope.randomizeAll();
+	});
+	// }}}
+});
